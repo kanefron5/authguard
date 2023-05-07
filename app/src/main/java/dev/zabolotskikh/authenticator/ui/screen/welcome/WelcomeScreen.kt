1290aab8e8d4f20d83bee5c00e16b2f525fd68c0
Roman Zabolotskikh <kanefron5@gmail.com>
2023-05-07 06:32:08 +0300

    Add application icon

--- app/src/main/java/dev/zabolotskikh/authenticator/ui/screen/welcome/WelcomeScreen.kt
+++ app/src/main/java/dev/zabolotskikh/authenticator/ui/screen/welcome/WelcomeScreen.kt
@@ -1,5 +1,6 @@
 package dev.zabolotskikh.authenticator.ui.screen.welcome
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,15 +9,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -34,13 +36,10 @@ fun WelcomeScreen() {
         verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Icon(
-            painter = painterResource(id = R.drawable.baseline_key_24),
+        Image(
+            painter = painterResource(id = R.drawable.app_logo),
             contentDescription = stringResource(id = R.string.app_logo_content_description),
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(150.dp),
-            tint = MaterialTheme.colorScheme.primary
+            modifier = Modifier.clip(RoundedCornerShape(40.dp))
         )
         Column(
             modifier = Modifier
