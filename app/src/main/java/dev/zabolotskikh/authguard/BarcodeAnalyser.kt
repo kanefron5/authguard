3a1d8e0d7319f0911c1ef3add4cf99dcd3a99886
Roman Zabolotskikh <kanefron5@gmail.com>
2023-05-08 03:54:05 +0300

    Add qr code scanner

--- app/src/main/java/dev/zabolotskikh/authguard/BarcodeAnalyser.kt
+++ app/src/main/java/dev/zabolotskikh/authguard/BarcodeAnalyser.kt
@@ -1,5 +1,6 @@
 package dev.zabolotskikh.authguard
 
+import android.net.Uri
 import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
@@ -7,31 +8,34 @@ import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
+import dev.zabolotskikh.authguard.domain.model.BarcodeService
 
 @ExperimentalGetImage
 class BarcodeAnalyser(
-    val callback: () -> Unit
+    val callback: (BarcodeService) -> Unit
 ) : ImageAnalysis.Analyzer {
     override fun analyze(imageProxy: ImageProxy) {
-        val options = BarcodeScannerOptions.Builder()
-            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
-            .build()
+        val options =
+            BarcodeScannerOptions.Builder().setBarcodeFormats(Barcode.FORMAT_QR_CODE).build()
 
         val scanner = BarcodeScanning.getClient(options)
         val mediaImage = imageProxy.image
         mediaImage?.let {
             val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
 
-            scanner.process(image)
-                .addOnSuccessListener { barcodes ->
-                    if (barcodes.size > 0) {
-                        callback()
+            scanner.process(image).addOnSuccessListener { barcodes ->
+                for (barcode: Barcode in barcodes) {
+                    barcode.rawValue?.apply {
+                        if (startsWith("otpauth://totp/")) {
+                            val uri = Uri.parse(this)
+                            val alias = uri.path?.removePrefix("/") ?: return@apply
+                            val secret = uri.getQueryParameter("secret") ?: return@apply
+                            val issuer = uri.getQueryParameter("issuer") ?: return@apply
+                            callback(BarcodeService(alias, secret, issuer))
+                        }
                     }
                 }
-                .addOnFailureListener {
-                    // Task failed with an exception
-                    // ...
-                }
+            }
         }
         imageProxy.close()
     }
