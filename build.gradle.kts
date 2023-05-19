90b26c2957f56f00e2210404cc1f650d11dc77ff
Roman Zabolotskikh <kanefron5@gmail.com>
2023-05-20 01:33:21 +0300

    #9: Migrate to version catalog

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+
+buildscript {
+    dependencies {
+        classpath(libs.dagger.hilt.plugin)
+    }
+}
+
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.dagger) apply false
+}
+true
