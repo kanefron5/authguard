9dbf0fa4c1bcc1e0d1694044cf3188b246955390
Roman Zabolotskikh <kanefron5@gmail.com>
2023-05-20 03:14:52 +0300

    #12: Add preview for all components in settings screen

--- app/src/main/java/dev/zabolotskikh/authguard/ui/screen/services/ServiceScreen.kt
+++ app/src/main/java/dev/zabolotskikh/authguard/ui/screen/services/ServiceScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.stringResource
