639ce5195ac4c7701c2c3d78fffda5aea7109ec3
Roman Zabolotskikh <kanefron5@gmail.com>
2023-05-21 01:25:57 +0300

    #13: Add Unit tests for viewmodels

---
+++ app/src/test/java/dev/zabolotskikh/authguard/data/TestAppStateRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package dev.zabolotskikh.authguard.data
+
+import dev.zabolotskikh.authguard.domain.model.AppState
+import dev.zabolotskikh.authguard.domain.repository.AppStateRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onEmpty
+import kotlinx.coroutines.flow.onStart
+
+class TestAppStateRepositoryImpl(
+    var state: AppState = AppState(
+        isStarted = false,
+        isAuthenticated = false,
+        isPrivateMode = false
+    )
+): AppStateRepository {
+    private val flow = MutableSharedFlow<AppState>()
+
+    override fun getState(): Flow<AppState> = flow.onStart { emit(state) }
+
+    override suspend fun update(appState: AppState) {
+        state = appState
+        flow.emit(state)
+    }
+}
