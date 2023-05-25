8a2fae0e6cad074c772a0a7037e4a2ce6bcc755c
Roman Zabolotskikh <kanefron5@gmail.com>
2023-05-25 18:55:21 +0300

    #19: Fix tests

--- app/src/test/java/dev/zabolotskikh/authguard/data/TestAppStateRepositoryImpl.kt
+++ app/src/test/java/dev/zabolotskikh/authguard/data/TestAppStateRepositoryImpl.kt
@@ -4,17 +4,13 @@ import dev.zabolotskikh.authguard.domain.model.AppState
 import dev.zabolotskikh.authguard.domain.repository.AppStateRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onEmpty
 import kotlinx.coroutines.flow.onStart
 
 class TestAppStateRepositoryImpl(
     var state: AppState = AppState(
         isStarted = false,
         isRemoteMode = false,
-        isPrivateMode = false,
-        passcode = null
+        isPrivateMode = false
     )
 ): AppStateRepository {
     private val flow = MutableSharedFlow<AppState>()
