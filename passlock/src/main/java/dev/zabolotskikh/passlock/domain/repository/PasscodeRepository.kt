ed99d8db62119882b2b5165f937a16bc6a05bc0a
Roman Zabolotskikh <kanefron5@gmail.com>
2023-05-25 20:09:07 +0300

    #19: Add passcode check; Move block logic to repository

--- passlock/src/main/java/dev/zabolotskikh/passlock/domain/repository/PasscodeRepository.kt
+++ passlock/src/main/java/dev/zabolotskikh/passlock/domain/repository/PasscodeRepository.kt
@@ -1,9 +1,10 @@
 package dev.zabolotskikh.passlock.domain.repository
 
+import dev.zabolotskikh.passlock.domain.model.PasscodeCheckStatus
 import kotlinx.coroutines.flow.Flow
 
 internal interface PasscodeRepository {
-    suspend fun checkPasscode(passcode: String): Boolean
+    suspend fun checkPasscode(passcode: String, maxAttemptCount: Int): PasscodeCheckStatus
 
     fun hasPasscode(): Flow<Boolean>
 
--- passlock/src/main/java/dev/zabolotskikh/passlock/ui/activity/PasscodeState.kt
+++ passlock/src/main/java/dev/zabolotskikh/passlock/ui/activity/PasscodeState.kt
@@ -3,8 +3,17 @@ package dev.zabolotskikh.passlock.ui.activity
 internal data class PasscodeState(
     val isConfirmed: Boolean = false,
     val isLimitReached: Boolean = false,
+    val isRejected: Boolean = false,
     val isSucceed: Boolean = false,
     val isCancelled: Boolean = false,
     val passcode: String = "",
     val attemptCount: Int = 0
+)
+
+internal fun PasscodeState.reset() = copy(
+    isConfirmed = false,
+    isLimitReached = false,
+    isSucceed = false,
+    isCancelled = false,
+    isRejected = false
 )
