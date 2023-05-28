8f86d783a8ea21d50052895433c2704622025093
Roman Zabolotskikh <kanefron5@gmail.com>
2023-05-29 00:01:45 +0300

    #19: Remove isPasscodeEnabled field from SettingsState

--- app/src/main/java/dev/zabolotskikh/authguard/ui/screen/settings/SettingsState.kt
+++ app/src/main/java/dev/zabolotskikh/authguard/ui/screen/settings/SettingsState.kt
@@ -1,6 +1,5 @@
 package dev.zabolotskikh.authguard.ui.screen.settings
 
 data class SettingsState(
-    val isPasscodeEnabled: Boolean = false,
     val currentSection: PreferenceSection = PreferenceSection.Main
 )
