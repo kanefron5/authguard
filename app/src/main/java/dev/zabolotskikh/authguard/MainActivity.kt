47e974dcb2175cb54ad2a756807a941535c1ab0d
Roman Zabolotskikh <kanefron5@gmail.com>
2023-06-12 02:51:41 +0300

    #31: Add splashscreen

--- app/src/main/java/dev/zabolotskikh/authguard/MainActivity.kt
+++ app/src/main/java/dev/zabolotskikh/authguard/MainActivity.kt
@@ -7,9 +7,15 @@ import androidx.camera.core.ExperimentalGetImage
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import dev.zabolotskikh.authguard.domain.model.AppState
 import dev.zabolotskikh.authguard.ui.MyAppNavHost
+import dev.zabolotskikh.authguard.ui.screen.welcome.WelcomeViewModel
 import dev.zabolotskikh.authguard.ui.theme.AuthGuardTheme
 import dev.zabolotskikh.passlock.ui.provider.PassLockProvider
 
@@ -17,6 +23,7 @@ import dev.zabolotskikh.passlock.ui.provider.PassLockProvider
 @ExperimentalGetImage
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             AuthGuardTheme {
@@ -25,7 +32,13 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxSize(),
                         color = MaterialTheme.colorScheme.background
                     ) {
-                        MyAppNavHost()
+                        val viewModel = hiltViewModel<WelcomeViewModel>()
+                        val state by viewModel.state.collectAsState()
+                        splashScreen.setKeepOnScreenCondition { state == null }
+
+                        state?.apply {
+                            MyAppNavHost(state = this)
+                        }
                     }
                 }
             }
