7e98353acffc5a7c1e855352549a4999aadc117a
Roman Zabolotskikh <kanefron5@gmail.com>
2023-06-18 03:53:44 +0300

    #18: Add tests for PasswordValidator

--- app/src/main/java/dev/zabolotskikh/authguard/data/repository/PasswordValidator.kt
+++ app/src/main/java/dev/zabolotskikh/authguard/data/repository/PasswordValidator.kt
@@ -5,6 +5,6 @@ import dev.zabolotskikh.authguard.domain.repository.DataValidator
 class PasswordValidator: DataValidator<String> {
     override fun check(input: String): Boolean {
         if (input.isEmpty()) return true
-        return input.matches(Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*+=()_\\-/\\\\\'\":;?]).{8,}$"))
+        return input.matches(Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[.,|`~\\[\\]<>{}!@#$%^&*+=()_\\-/\\\\\'\":;?]).{8,}$"))
     }
 }
