b62e398fc9800204fe75802c2b0147a2d2de3fcc
Roman Zabolotskikh <kanefron5@gmail.com>
2023-06-18 03:53:44 +0300

    #18: Add tests for PasswordValidator

---
+++ app/src/test/java/dev/zabolotskikh/authguard/data/PasswordValidatorTests.kt
@@ -0,0 +1,92 @@
+package dev.zabolotskikh.authguard.data
+
+import dev.zabolotskikh.authguard.data.repository.PasswordValidator
+import org.junit.Assert
+import org.junit.Test
+
+class PasswordValidatorTests {
+    @Test
+    fun `test empty`() {
+        Assert.assertTrue(PasswordValidator().check(""))
+    }
+
+    @Test
+    fun `test less 8 symbols`() {
+        Assert.assertFalse(PasswordValidator().check("Qq!4567"))
+        Assert.assertFalse(PasswordValidator().check("1"))
+    }
+
+    @Test
+    fun `test correct`() {
+        Assert.assertTrue(PasswordValidator().check("Qq!45678"))
+    }
+
+    @Test
+    fun `test symbols`() {
+        val symbols = arrayOf(
+            '!',
+            '@',
+            '#',
+            '$',
+            '%',
+            '^',
+            '&',
+            '*',
+            '(',
+            ')',
+            '-',
+            '_',
+            '+',
+            '=',
+            ';',
+            ':',
+            '\'',
+            '"',
+            '\\',
+            '/',
+            '|',
+            '?',
+            '`',
+            '~',
+            '[',
+            ']',
+            '{',
+            '}',
+            '<',
+            '>',
+            ',',
+            '.'
+        )
+        symbols.forEach {
+            val s = "Qq${it}45678"
+            println(s)
+            Assert.assertTrue(PasswordValidator().check(s))
+        }
+    }
+
+    @Test
+    fun testNumbers() {
+        repeat(10) {
+            val s = "Qq${it}!qqqqqq"
+            println(s)
+            Assert.assertTrue(PasswordValidator().check(s))
+        }
+    }
+
+    @Test
+    fun testLowercase() {
+        for (i in 97..122) {
+            val s = "QQ${i.toChar()}!1QQQQQ"
+            println(s)
+            Assert.assertTrue(PasswordValidator().check(s))
+        }
+    }
+    @Test
+    fun testUppercase() {
+        for (i in 65..90) {
+            val s = "qq${i.toChar()}!1qqqqq"
+            println(s)
+            Assert.assertTrue(PasswordValidator().check(s))
+        }
+    }
+}
